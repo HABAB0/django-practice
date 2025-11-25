6c7a9b5482e6dfefe0f809ceccdc646f516c5f0c
gr434_shaaal <tolya.shaprove@mail.ru>
2025-11-25 11:41:35 +0700

    create home page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    num_genres = Genre.objects.count()
+    num_books_War = Book.objects.filter(title__icontains='War').count()
+
+
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books':num_books,
+            'num_instances':num_instances,
+            'num_instances_available':num_instances_available,
+            'num_authors':num_authors,
+            'num_genres':num_genres,
+            'num_books_War':num_books_War
+        },
+    )
 
 # Create your views here.
