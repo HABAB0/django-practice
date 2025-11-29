31e0e4d4f5acbe6f9d22e2f0e27535af595beb06
gr434_shaaal <tolya.sharove@mail.ru>
2025-11-29 11:42:21 +0700

    add: user model

--- interior/models.py
+++ interior/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    FIO = models.CharField(max_length=100 , blank=False, null=False),
+    agree_personal_data = models.BooleanField(default=False)
+
+
--- interior/views.py
+++ interior/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
 
-# Create your views here.
+from django.shortcuts import render
+from .models import *
+
+from interior.models import User
+
+
+def index(request):
+    num_users = User.objects.count()
+
+    context = {'num_users': num_users}
+
+    return render(request, 'interior/index.html', context)
